fcb681822b084c263d2c22441fa880bc04d3925a
guboc11 <guboc11@gmail.com>
2023-11-10 05:29:36 +0900

    fix identity

--- src/declarations/counter/counter.did.js
+++ src/declarations/counter/counter.did.js
@@ -2,6 +2,7 @@ export const idlFactory = ({ IDL }) => {
   return IDL.Service({
     'getValue' : IDL.Func([], [IDL.Nat], ['query']),
     'increment' : IDL.Func([], [], []),
+    'whoami' : IDL.Func([], [IDL.Principal], []),
   });
 };
 export const init = ({ IDL }) => { return []; };
