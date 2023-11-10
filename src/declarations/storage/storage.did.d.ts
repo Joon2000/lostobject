991b28f40f85f45ebc3ff3f29accfddb8a39bbde
Joon2000 <najoonwon22@gmail.com>
2023-11-10 09:46:59 +0900

    Add storage motoko

---
+++ src/declarations/storage/storage.did.d.ts
@@ -0,0 +1,7 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface _SERVICE {
+  'get' : ActorMethod<[string], string>,
+  'store' : ActorMethod<[string, string], undefined>,
+}
